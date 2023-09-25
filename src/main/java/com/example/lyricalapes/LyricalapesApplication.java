d8eae61f47fd7c09e01b4becb95a5aa4ee71c90d
Rick Morales <rick.morales7693@gmail.com>
2023-09-25 15:19:57 -0500

    second commit

---
+++ src/main/java/com/example/lyricalapes/LyricalapesApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyricalapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricalapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricalapesApplication.class, args);
+	}
+
+}
