881e3f87ab5cdd55266b9a93a650236413c8559f
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-09-28 13:40:50 -0500

    added all getters&setters and constructors too all Entities

--- src/main/java/com/example/lyricalapes/models/Like.java
+++ src/main/java/com/example/lyricalapes/models/Like.java
@@ -17,4 +17,39 @@ public class Like {
     @JoinColumn(name = "verse_id")
     private Verse verse;
 
+    ///////CONSTRUCTORS ////////
+    public Like() {
+    }
+
+    public Like(long id, User user, Verse verse) {
+        this.id = id;
+        this.user = user;
+        this.verse = verse;
+    }
+
+    //////GETTER & SETTER//////
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Verse getVerse() {
+        return verse;
+    }
+
+    public void setVerse(Verse verse) {
+        this.verse = verse;
+    }
 }
