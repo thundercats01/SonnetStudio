e66cef2d052e06f3d1a258953948ce36f56a3f23
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-10-02 15:20:43 -0500

    created the create a verse controller, failed trying to make the edit-profile controller

---
+++ src/main/java/com/example/lyricalapes/controllers/EditProfileController.java
@@ -0,0 +1,17 @@
+//package com.example.lyricalapes.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/")
+//public class EditProfileController {
+//
+//
+//    @GetMapping("edit-profile")
+//    public String showEditProfile() {
+//        return "profile/editprofile";
+//    }
+//
+//}
