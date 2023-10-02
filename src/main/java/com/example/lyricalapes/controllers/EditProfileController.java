29ac641c9534ff61da6a46095d1bb6c4b4569947
Kurt Suttin <Kurtlesuttin@gmail.com>
2023-10-02 15:49:01 -0500

    edit profile view works now

--- src/main/java/com/example/lyricalapes/controllers/EditProfileController.java
+++ src/main/java/com/example/lyricalapes/controllers/EditProfileController.java
@@ -1,17 +1,32 @@
-//package com.example.lyricalapes.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/")
-//public class EditProfileController {
-//
-//
-//    @GetMapping("edit-profile")
-//    public String showEditProfile() {
-//        return "profile/editprofile";
-//    }
-//
-//}
+package com.example.lyricalapes.controllers;
+
+import com.example.lyricalapes.models.User;
+import com.example.lyricalapes.repositories.UserRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class EditProfileController {
+    UserRepo userRepo;
+
+    public EditProfileController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+
+
+    @GetMapping("edit-profile")
+    public String showEditProfile(Model model) {
+        User loggedInPrinciple = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User loggedInUser = userRepo.findByUsername(loggedInPrinciple.getUsername());
+        model.addAttribute("user",loggedInUser);
+
+        return "profile/editprofile";
+
+    }
+
+}
