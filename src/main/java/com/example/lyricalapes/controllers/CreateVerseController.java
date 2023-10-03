3ca8d553c5807aa892d43d3ce79725d563a7cbf4
Jesse <Jdorman1289@gmail.com>
2023-10-03 10:16:29 -0500

    create post mapping for create verse

--- src/main/java/com/example/lyricalapes/controllers/CreateVerseController.java
+++ src/main/java/com/example/lyricalapes/controllers/CreateVerseController.java
@@ -1,18 +1,26 @@
 package com.example.lyricalapes.controllers;
 
+import com.example.lyricalapes.models.User;
+import com.example.lyricalapes.models.Verse;
 import com.example.lyricalapes.repositories.UserRepo;
+import com.example.lyricalapes.repositories.VerseRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class CreateVerseController {
 
     UserRepo usersDAO;
+    VerseRepo versesDAO;
 
-    public CreateVerseController(UserRepo usersDAO) {
+    public CreateVerseController(UserRepo usersDAO, VerseRepo versesDAO) {
         this.usersDAO = usersDAO;
+        this.versesDAO = versesDAO;
     }
 
     @GetMapping("/create")
@@ -20,7 +28,20 @@ public class CreateVerseController {
         return "profile/createverse";
     }
 
-//    @PostMapping()
+    //    @PostMapping()
     // user can actually create a post and it get saved to the database
+    @PostMapping("/create")
+    public String getVerseParams(@RequestParam String verseText) {
+        User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Verse verse = new Verse();
+        verse.setUser(loggedInUser);
+        verse.setContent(verseText);
+
+        versesDAO.save(verse);
+        return "redirect:/profile";
+
+    }
+
 
 }
