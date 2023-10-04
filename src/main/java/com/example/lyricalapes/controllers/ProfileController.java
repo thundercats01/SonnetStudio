12a2ca28267382be56f0e14543cdf5f20e78e00a
Damian Solis <damianrsolis@gmail.com>
2023-10-04 14:19:34 -0500

    profileview posts display logged in user post

--- src/main/java/com/example/lyricalapes/controllers/ProfileController.java
+++ src/main/java/com/example/lyricalapes/controllers/ProfileController.java
@@ -1,7 +1,10 @@
 package com.example.lyricalapes.controllers;
 
 import com.example.lyricalapes.models.User;
+import com.example.lyricalapes.repositories.CommentRepo;
 import com.example.lyricalapes.repositories.UserRepo;
+import com.example.lyricalapes.repositories.VerseRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +16,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProfileController {
 
     private UserRepo usersDAO;
+    private VerseRepo verseRepo;
+    private CommentRepo commentRepo;
 
-
-    public ProfileController(UserRepo usersDAO) {
+    public ProfileController(UserRepo usersDAO, VerseRepo verseRepo, CommentRepo commentRepo) {
         this.usersDAO = usersDAO;
+        this.verseRepo = verseRepo;
+        this.commentRepo = commentRepo;
     }
 
     @GetMapping("/profile")
-    public String showProfile() {
+    public String showProfile(Model model) {
+        // Get the logged-in user
+        User loggedInPrinciple = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User loggedInUser = usersDAO.findByUsername(loggedInPrinciple.getUsername());
+
+        // Fetch posts and comments made by the logged-in user
+        model.addAttribute("verses", verseRepo.findAllByUser(loggedInUser));
+        model.addAttribute("comments", commentRepo.findAll());
+
         return "profile/profileview";
     }
+
 
     @GetMapping("/profile/{id}")
     public String ShowOtherUserProfile(@PathVariable Long id, Model model) {
         User clickedUser = usersDAO.findById(id).get();
-        model.addAttribute("user",clickedUser);
+        model.addAttribute("user", clickedUser);
         return "profile/usersprofileview";
     }
 
