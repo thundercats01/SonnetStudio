f5bad815ddc4423407773dc7253b7efb5081fb70
Rick Morales <rick.morales7693@gmail.com>
2023-10-12 11:57:35 -0500

    fixed the meettheteam redirect

--- src/main/java/com/example/lyricalapes/controllers/MeetTheTeamController.java
+++ src/main/java/com/example/lyricalapes/controllers/MeetTheTeamController.java
@@ -1,10 +1,12 @@
 package com.example.lyricalapes.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
-@RequestMapping("/meettheteam")
+@RequestMapping("/")
+@Controller
 public class MeetTheTeamController {
 
     @GetMapping("/meettheteam")
