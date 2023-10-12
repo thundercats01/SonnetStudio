173b7b4bbf0d1e92f50d4a512af5c00867cffc92
Damian Solis <damianrsolis@gmail.com>
2023-10-12 11:58:50 -0500

    corrected likes mapping

--- src/main/java/com/example/lyricalapes/services/SecurityConfiguration.java
+++ src/main/java/com/example/lyricalapes/services/SecurityConfiguration.java
@@ -35,7 +35,7 @@ public class SecurityConfiguration {
         http.authorizeHttpRequests((requests) -> requests
                         /* Pages that require authentication
                          * only authenticated users can create and edit ads */
-                        .requestMatchers("/explore", "/profile", "/profile/**","/badge","/like","/create/**","/edit-profile","/comments/**", "/post_delete","edit-user-profile","/follow","/searchUsers","/apple-badge","/default-badge","/einstein-badge","/joan-badge","/masterkey-badge","/pearl-badge","/socrates-badge","/king-badge", "/keys.js").authenticated()
+                        .requestMatchers("/explore", "/profile", "/profile/**","/badge","/like","/like/**","/create/**","/edit-profile","/comments/**", "/post_delete","edit-user-profile","/follow","/searchUsers","/apple-badge","/default-badge","/einstein-badge","/joan-badge","/masterkey-badge","/pearl-badge","/socrates-badge","/king-badge", "/keys.js").authenticated()
                         /* Pages that do not require authentication
                          * anyone can visit the home page, register, login, and view ads */
                         .requestMatchers("/", "/sign-up", "/login").permitAll()
